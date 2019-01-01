4f57ef8785fdf5b88d06e8e258c64ace33562a13
Peter <45145046+afghWp@users.noreply.github.com>
2019-01-01 17:56:59 +0100

    Update zaloguj.php

--- zaloguj.php
+++ zaloguj.php
@@ -1,10 +1,25 @@
 <?php
+	require_once "conect.php"; 
+	//uzywamy funkcji require_once by tylko raz wykonac czynnosc logowania i ze stopowaniem gdy mamy blad 
+	
+	
+	$polaczenie = new mysqli($host,$db_user,$db_password,$db_name);
+	//tworzymy zmienna-obiekt poprzez new bedaca polaczeniem, uzywamy nowej, wspieranej funkcji/konstruktora mysqli kltora laczy baze $host...
+	
+	if ($polaczenie->connect_errno!=0) //nalezy sprawdzic czy jest polaczenie, jak nie ma to, przejmujemy obsluge bledu
+	//jak jest polaczenie to connect_errno jest 0
+	{
+		echo " Jest blad Error: ".$polaczenie->connect_errno." Opis bledu: ".$polaczenie->connect_error;
+	}
+	else
+	{
+		echo "Connect works!!";
+		$login=$_POST['login']; //wpisujemy badane login i haslo
+		$haslo=$_POST['haslo'];
 
-echo $_POST['login']; //sczytujemy wartosci z tabeli POST gdyby było get to z GET
-echo "<br/>";
-echo $_POST['haslo'];
+		echo $login."_  ".$haslo."<br/>";
+		
+		$polaczenie->close(); //nalezy zamknac polaczenie
+	}
 
-
-
-
-?> 
+?>

034e6ea518c2342bc9e01a1148630ad00af8d8fd
Peter <45145046+afghWp@users.noreply.github.com>
2019-01-01 12:10:49 +0100

    Update index.php
    zmiana

--- index.php
+++ index.php
@@ -3,9 +3,23 @@
 <head>
 	<meta charset="utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<title>moja gra pod tytułem "osadnicy"</title>
 </head>
 
 <body>
+	<h4>
+		Ludzie chcą, by historia była do nich podobna, względnie żeby była podobna przynajmniej do ich snów.<br/>
+		Na szczęście zdarza im się czasem, że mają wielkie sny.<br/>
+		<h5>Charles de Gaulle</h5>
+	</h4> <br/><br/>
+	
+	<form>
+		Podaj login:
+		
+		Podaj hasło:
+	
+	</form>
+
 
 </body>
-</html>  
+</html>

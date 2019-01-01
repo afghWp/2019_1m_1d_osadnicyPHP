18dda6ed158ee490d30a609c0ffceca2f6f6e9e0
Peter <45145046+afghWp@users.noreply.github.com>
2019-01-01 12:37:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,15 +11,16 @@
 		Ludzie chcą, by historia była do nich podobna, względnie żeby była podobna przynajmniej do ich snów.<br/>
 		Na szczęście zdarza im się czasem, że mają wielkie sny.<br/>
 		<h5>Charles de Gaulle</h5>
-	</h4> <br/><br/>
+	</h4> <br/>
 	
-	<form>
-		Podaj login:
-		
-		Podaj hasło:
-	
+	<!--tworzymy formularz form dlatego by miec blok i moc przeslac z jego dane do drugiej strony zaloguj.php metoda post-schowanym tekstem-->
+	<form action="zaloguj.php" method="post">
+		Podaj login:  <input type="text" name="login" /> <br/> <br/>
+		<!-- wczytujemy dane poprzez pola input login odkrytym tekstem, haslo maskowane-->
+		Podaj hasło:  <input type="password" name="haslo" /> <br/>
+		<input type="submit" value="Zaloguj się" /> <!--submit to przycisk z napisem value -->
 	</form>
 
 
 </body>
-</html>
+</html> 
